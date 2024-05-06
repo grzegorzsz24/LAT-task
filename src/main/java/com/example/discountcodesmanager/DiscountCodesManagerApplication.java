726907edc5cf58ed141534873c147a59f6a182ae
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-06 17:11:12 +0200

    Initial commit

---
+++ src/main/java/com/example/discountcodesmanager/DiscountCodesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.discountcodesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCodesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountCodesManagerApplication.class, args);
+	}
+
+}
