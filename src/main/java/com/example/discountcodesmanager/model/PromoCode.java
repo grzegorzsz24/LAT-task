a3e360ac67b826951c1e9468009fbfce3ffec8cd
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-11 22:20:29 +0200

    Add promo code repository tests

--- src/main/java/com/example/discountcodesmanager/model/PromoCode.java
+++ src/main/java/com/example/discountcodesmanager/model/PromoCode.java
@@ -2,6 +2,7 @@ package com.example.discountcodesmanager.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -11,6 +12,7 @@ import java.util.Currency;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class PromoCode {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +26,13 @@ public class PromoCode {
     private Integer maxUsages;
     private Integer currentUsages;
 
+    public PromoCode(DiscountType discountType, String code, LocalDateTime expirationDate, BigDecimal discountAmount, Currency discountCurrency, Integer maxUsages, Integer currentUsages) {
+        this.discountType = discountType;
+        this.code = code;
+        this.expirationDate = expirationDate;
+        this.discountAmount = discountAmount;
+        this.discountCurrency = discountCurrency;
+        this.maxUsages = maxUsages;
+        this.currentUsages = currentUsages;
+    }
 }
